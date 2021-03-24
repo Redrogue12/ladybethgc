5dade8f359e21321342576e9def3598a2307b4cd
Redrogue12 <exgc12@gmail.com>
2021-03-23 23:40:17 -0400

    init navigation bar

---
+++ components/Header.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink
+} from 'reactstrap';
+
+import styles from '../styles/Navbar.module.css';
+
+const NavbarHeader = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <div>
+          <NavbarBrand href="/">Ladybeth González-Cortés, LLC</NavbarBrand>
+          <p className={styles.navbarSubtitle}>Psychologist, Yoga & Meditation Instructor</p>
+        </div>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="/">Home</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/about">About</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/blog">Blog</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/services">Services</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/contact">Contact</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+export default NavbarHeader;
