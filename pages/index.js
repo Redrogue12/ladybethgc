c771cca191ffcbf70f5599fc01889b0a34c50c04
Redrogue12 <exgc12@gmail.com>
2021-05-24 00:05:52 -0400

    add basic components for home

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import Head from 'next/head'
-import Link from 'next/link'
-import styles from '../styles/Home.module.css'
+// import Link from 'next/link'
+import HeroArea from '../components/HeroArea'
+import About from '../components/About'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="">
       <Head>
         <title>Ladybeth González-Cortés, LCC</title>
         <link rel="icon" href="/favicon.ico" />
@@ -12,27 +13,10 @@ export default function Home() {
         <meta charSet="utf-8" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>Welcome to our demo blog!</h1>
-
-        <p>
-          You can find more articles on the{' '}
-          <Link href='/blog'>
-            <a>blog articles page</a>
-          </Link>
-        </p>
+      <main className="">
+        <HeroArea />
+        <About />
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }
