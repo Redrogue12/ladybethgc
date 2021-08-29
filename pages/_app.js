23ad2c51b22f0e8cde901d16e7f7278b85c4000a
Redrogue12 <exgc12@gmail.com>
2021-08-28 18:22:38 -0600

    add about area styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/styles.css'
 import '../styles/hero-area.css'
+import '../styles/about-area.css'
 import 'font-awesome/css/font-awesome.min.css'
 import Header from '../components/Header';
 
