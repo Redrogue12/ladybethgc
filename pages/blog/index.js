151e28a4c7c48efe2c6d70ea07bed91f0868ce11
Redrogue12 <exgc12@gmail.com>
2021-08-29 16:02:56 -0600

    style blog

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -3,34 +3,44 @@ import Link from 'next/link';
 
 import { getAllPosts } from '../../lib/api';
 
+const formatDate = date => {
+  const newDate = new Date(date);
+
+  return `${newDate.getDate()}/${
+    newDate.getMonth() + 1
+  }/${newDate.getFullYear()}`
+};
+
 const Blog = ({ allPosts: { edges } }) => {
   return (
-    <div className="">
+    <div className="blog-page">
       <Head>
         <title>Blog articles page</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <main className="">
-        <h1 className="">Latest blog articles</h1>
-        <hr />
-        <section>
+        <h1 className="text-center">Latest blog articles</h1>
+        <section className="articles">
           {edges.map(({ node }) => (
-            <div className="" key={node.id}>
-              <div className="">
+            <div className="article" key={node.id}>
+              <div className="article-img-container">
                 <figure>
                   <img
-                    style={{ marginRight: 20 }}
+                    className="article-img"
                     src={node.extraPostInfo?.thumbImage?.mediaItemUrl}
                     alt={node.title}
                   />
                 </figure>
               </div>
               <div className="">
-                <h2>{node.title}</h2>
-                <p>{node.extraPostInfo?.authorExcerpt}</p>
                 <Link href={`/blog/${node.slug}`}>
-                  <a>Read more</a>
+                  <h2 className="pointer primary-color">{node.title}</h2>
+                </Link>
+                <p className="primary-color">{formatDate(node.date)}</p>
+                <p className="primary-color">{node.extraPostInfo?.authorExcerpt}</p>
+                <Link href={`/blog/${node.slug}`}>
+                  <a className="secondary-color">Read more</a>
                 </Link>
               </div>
             </div>
