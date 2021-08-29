f7b19a65236d91c986bf0eee233c84292d562f6c
Redrogue12 <exgc12@gmail.com>
2021-08-29 16:15:07 -0600

    services styles

--- pages/services/index.js
+++ pages/services/index.js
@@ -1,22 +1,22 @@
 import Head from 'next/head';
 
-const Blog = () => {
+const Services = () => {
   return (
-    <div className="">
+    <div className="p-40 primary-bg vh-100">
       <Head>
         <title>Services</title>
         <link rel='icon' href='/favicon.ico' />
         <script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script>
       </Head>
 
-      <main className="h-100">
-        <h1 className="">Services</h1>
+      <main>
+        <h1 className="text-center primary-color">Services</h1>
         <section className="calendly-section">
-          <div className="calendly-inline-widget" data-url="https://calendly.com/egonzalezcortes"></div>
+          <div className="calendly-inline-widget" data-url="https://calendly.com/psicologalgc/"></div>
         </section>
       </main>
     </div>
   )
 };
 
-export default Blog
+export default Services;
