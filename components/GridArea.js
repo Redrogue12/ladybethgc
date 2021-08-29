615942d45a2cff3670926b6252383547fe02a563
Redrogue12 <exgc12@gmail.com>
2021-08-28 18:22:55 -0600

    init

---
+++ components/GridArea.js
@@ -0,0 +1,22 @@
+import { Row, Col } from "reactstrap";
+
+const GridArea = () => {
+  return (
+    <Row className="">
+      <Col className="" xl="" lg="" md="" sm="1">
+       
+      </Col>
+      <Col className="" xl="" lg="" md="" sm="1">
+        
+      </Col>
+      <Col className="" xl="" lg="" md="" sm="1">
+       
+      </Col>
+      <Col className="" xl="" lg="" md="" sm="1">
+
+      </Col>
+    </Row>
+  );
+}
+
+export default GridArea;
