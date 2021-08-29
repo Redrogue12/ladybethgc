cb81ea267d39f331d9666329c8b54adcf53c262d
Redrogue12 <exgc12@gmail.com>
2021-08-28 19:20:00 -0600

    responsiveness

--- components/GridArea.js
+++ components/GridArea.js
@@ -3,7 +3,7 @@ import { Row, Col } from "reactstrap";
 const GridArea = () => {
   return (
     <Row className="grid-row">
-      <Col className="grid-col" xl="6" lg="6" md="6" sm="12">
+      <Col className="grid-col grid-col-1" xl="6" lg="6" md="6" sm="12">
        <h3>Recent Projects</h3>
        <p>Having meaningful, unique branding has the power to align your business with the right people. Explore some recently celebrated projects and get inspired to start your own.</p>
        <a href="">Visit Portfolio</a>
@@ -14,7 +14,7 @@ const GridArea = () => {
       <Col className="grid-col grid-col-3" xl="6" lg="6" md="6" sm="12">
        
       </Col>
-      <Col className="grid-col" xl="6" lg="6" md="6" sm="12">
+      <Col className="grid-col grid-col-4" xl="6" lg="6" md="6" sm="12">
         <h3>Services</h3>
         <p>No matter the project, you'll be guided through every step of the design process. Ready to take the first step towards holistically transforming your business?</p>
         <a href="">View Offerings</a>
