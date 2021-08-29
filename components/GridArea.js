042649c1295ab7947397d81fd2dcf6a1aca4ad4a
Redrogue12 <exgc12@gmail.com>
2021-08-28 19:07:09 -0600

    populate grids

--- components/GridArea.js
+++ components/GridArea.js
@@ -2,18 +2,22 @@ import { Row, Col } from "reactstrap";
 
 const GridArea = () => {
   return (
-    <Row className="">
-      <Col className="" xl="" lg="" md="" sm="1">
-       
+    <Row className="grid-row">
+      <Col className="grid-col" xl="6" lg="6" md="6" sm="12">
+       <h3>Recent Projects</h3>
+       <p>Having meaningful, unique branding has the power to align your business with the right people. Explore some recently celebrated projects and get inspired to start your own.</p>
+       <a href="">Visit Portfolio</a>
       </Col>
-      <Col className="" xl="" lg="" md="" sm="1">
+      <Col className="grid-col grid-col-2" xl="6" lg="6" md="6" sm="12">
         
       </Col>
-      <Col className="" xl="" lg="" md="" sm="1">
+      <Col className="grid-col grid-col-3" xl="6" lg="6" md="6" sm="12">
        
       </Col>
-      <Col className="" xl="" lg="" md="" sm="1">
-
+      <Col className="grid-col" xl="6" lg="6" md="6" sm="12">
+        <h3>Services</h3>
+        <p>No matter the project, you'll be guided through every step of the design process. Ready to take the first step towards holistically transforming your business?</p>
+        <a href="">View Offerings</a>
       </Col>
     </Row>
   );
