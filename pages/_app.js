0608348976e09312ea0179f505f168372fab5333
Redrogue12 <exgc12@gmail.com>
2021-08-29 16:14:53 -0600

    add servies css

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../styles/styles.css'
-import '../styles/hero-area.css'
-import '../styles/about-area.css'
-import '../styles/grid-area.css'
-import '../styles/blog.css'
-import 'font-awesome/css/font-awesome.min.css'
+import '../styles/styles.css';
+import '../styles/hero-area.css';
+import '../styles/about-area.css';
+import '../styles/grid-area.css';
+import '../styles/blog.css';
+import '../styles/services.css';
+import 'font-awesome/css/font-awesome.min.css';
 import Header from '../components/Header';
 
 function MyApp({ Component, pageProps }) {
