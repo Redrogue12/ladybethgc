4d3713fb4779ab2dbfea1c81b536e63a0e66e8d6
Redrogue12 <exgc12@gmail.com>
2021-08-28 19:07:15 -0600

    add grid styles

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/styles.css'
 import '../styles/hero-area.css'
 import '../styles/about-area.css'
+import '../styles/grid-area.css'
 import 'font-awesome/css/font-awesome.min.css'
 import Header from '../components/Header';
 
