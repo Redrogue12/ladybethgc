d66abb3727803024387324262f70c524815a6338
Redrogue12 <exgc12@gmail.com>
2021-09-10 17:59:39 -0600

    temp solution

--- components/IgFeed.js
+++ components/IgFeed.js
@@ -6,7 +6,7 @@ export default function IgFeed () {
   return (
     <div className="p-40 text-center ig-feed">
       <a href="https://www.instagram.com/psic.ladybethgc/" className="ig-link">@PSIC.LADYBETHGC</a>
-      <InstagramFeed token={process.env.NEXT_PUBLIC_IG_TOKEN} counter="6"/>
+      {/* <InstagramFeed token={process.env.NEXT_PUBLIC_IG_TOKEN} counter="6"/> */}
     </div>
   );
 };
