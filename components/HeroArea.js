5bb7a4491e1288bff9d7bfeed9f9c027354405ca
Redrogue12 <exgc12@gmail.com>
2022-09-02 19:12:36 -0600

    add border radius to about + hero area

--- components/HeroArea.js
+++ components/HeroArea.js
@@ -6,7 +6,7 @@ const HeroArea = () => {
       <Col className="col1" xl="8" lg="6" md="6" sm="12">
         <h2>Terapia Psicológica genuina, amable y compasiva</h2>
         <p>Apoyándote a crear una vida plena y presente</p>
-        <a href="">Ver Servicios</a>
+        <a href="/services">Ver Servicios</a>
       </Col>
       <Col className="col2" xl="4" lg="6" md="6" sm="12" />
     </Row>
